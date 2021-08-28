9f5c2966d8211b46d161a24dbc87534c3ae6ca69
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-28 13:32:28 -0400

    updated get and post routes to read from db. also added functionality for delete requests

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -6,12 +6,23 @@ const { v4: uuidv4 } = require('uuid');
 
 const db = require("../../db/db.json");
 
+let readNotes = () => {
+    const notes = fs.readFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        'utf-8'
+    );
+
+    return JSON.parse(notes);
+}
+
 router
     .route('/notes')
     .get((req, res) => {
+        let db = readNotes()
         res.json(db);
     })
     .post((req, res) => {
+        let db = readNotes()
         // add unique id
         req.body.id = uuidv4();
         // update db
@@ -22,8 +33,24 @@ router
             JSON.stringify(db, null, 2)
         );
         // render response on screen
-        res.render(res)
+        res.json(req.body)
 
-    });
+    })
+
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let db = readNotes()
+    let deletedDb = db.filter(element => {
+        return element.id != id;
+    })
+
+    fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify(deletedDb, null, 2)
+    );
+
+    res.json({ ok: true });
+
+})
 
 module.exports = router;
