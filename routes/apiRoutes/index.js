5ef777726873d943a04e050a09720e221f3bd1b0
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-28 11:32:47 -0400

    added post request for notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,29 @@
 const path = require('path');
+const fs = require("fs");
+
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 
 const db = require("../../db/db.json");
 
-router.get('/notes', (req, res) => {
-    console.log(db)
-    res.json(db);
-});
+router
+    .route('/notes')
+    .get((req, res) => {
+        res.json(db);
+    })
+    .post((req, res) => {
+        // add unique id
+        req.body.id = uuidv4();
+        // update db
+        db.push(req.body);
+        // write to db
+        fs.writeFileSync(
+            path.join(__dirname, "../../db/db.json"),
+            JSON.stringify(db, null, 2)
+        );
+        // render response on screen
+        res.render(res)
+
+    });
 
 module.exports = router;
