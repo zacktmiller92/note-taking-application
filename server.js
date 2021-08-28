50645139ec6173f1d543019a955863718e60d32a
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-28 10:20:12 -0400

    added routes folder and html get requests for notes and index pages

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+
+const PORT = process.env.PORT || 3001;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
