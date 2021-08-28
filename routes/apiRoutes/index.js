2cb8bcac906cc3f597e3679d4a13f1be227fecc4
zacktmiller92 <zacktmiller92@gmail.com>
2021-08-28 10:44:17 -0400

    added html and api get request routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,4 +1,11 @@
+const path = require('path');
 const router = require('express').Router();
 
+const db = require("../../db/db.json");
+
+router.get('/notes', (req, res) => {
+    console.log(db)
+    res.json(db);
+});
 
 module.exports = router;
